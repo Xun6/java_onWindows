69cfed48cb42e90ce2f1c88407d7b8fa9bb7e7ca
Xun6 <13617920171@163.com>
2021-05-10 17:35:41 +0800

    update

--- xiaoyu/FileIOStudy/ModifyFileName.java
+++ xiaoyu/FileIOStudy/ModifyFileName.java
@@ -1,10 +1,6 @@
 package xiaoyu.FileIOStudy;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * 修改文件名示例
@@ -14,23 +10,24 @@ public class ModifyFileName {
         String realPath = "G:\\local_pic\\172.18.30.55\\1";
 //        String folderPath = realPath.substring(0, realPath.lastIndexOf("\\"));
         File file = new File(realPath);
+        System.out.println(realPath);
         String dirPath = file.getAbsolutePath(); //目录绝对路径
+        System.out.println(dirPath);
         int num = 1;
         if (file.isDirectory())
         {
-//            List<File> files = Arrays.asList(file.listFiles()); // 获取此目录下的文件列表
             File[] files = file.listFiles();//获取此目录下的文件列表
-//            System.out.println(files.toString());
             for (File fileFrom : files) {
-                String fromFiledir = fileFrom.getName();//得到单个文件名
-                System.out.println(fromFiledir);
-                 //判断存在同一后缀名的文件
-                if (!fromFiledir.isEmpty()) {
-//                    fromFile = fromFile.substring(0, fromFile.lastIndexOf("."));
-                    String fromFile = "1-1." + num + ".jpg";
-                    String toFileName = realPath + "\\" + fromFile;  // 重命名拼接
+                String fromFile = fileFrom.getName();//得到单个文件名
+                System.out.println(fromFile);
+//                fromFile = fromFile.substring(0,fromFile.lastIndexOf("_"));
+//                System.out.println(fromFile);
+                // 判断存在同一后缀名的文件
+                if (fromFile.endsWith(".jpg")) {
+                    fromFile = fromFile.substring(0, fromFile.lastIndexOf("_"));
+                    String toFileName = dirPath + "\\" + fromFile + "1-1."+num +".jpg";  // 重命名拼接
                     File toFile = new File(toFileName);
-                    if (fileFrom.exists() && !fileFrom.equals(fromFile)) {
+                    if (fileFrom.exists() && !toFile.exists()) {
                         //开始更名
                         fileFrom.renameTo(toFile);
                         System.out.println("名称修改成功！");
@@ -39,7 +36,7 @@ public class ModifyFileName {
                     }
                 }
                 num++;
-                if(num > 2){
+                if(num == 2){
                     num = 1;
                 }
             }
